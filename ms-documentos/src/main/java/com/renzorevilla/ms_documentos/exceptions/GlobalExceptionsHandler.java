36e3b42cb51d24098861f34628ad969fd46afd6e
Renzo Revilla <renzorevq@gmail.com>
2026-02-08 20:28:18 -0500

    Exceptions: mensaje de error agregado

--- ms-documentos/src/main/java/com/renzorevilla/ms_documentos/exceptions/GlobalExceptionsHandler.java
+++ ms-documentos/src/main/java/com/renzorevilla/ms_documentos/exceptions/GlobalExceptionsHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,4 +23,12 @@ public class GlobalExceptionsHandler {
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("mensaje", ex.getReason());
+        return ResponseEntity.status(ex.getStatusCode()).body(error);
+    }
+
 }
